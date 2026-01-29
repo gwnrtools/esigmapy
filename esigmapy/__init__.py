564e546e2ea3f57acdd8022ebf7cc156f2de9777
Alexander Harvey Nitz <alex.nitz@gmail.com>
2026-01-29 15:08:20 -0500

    allow option for no merger

--- esigmapy/__init__.py
+++ esigmapy/__init__.py
@@ -23,7 +23,12 @@ _params = inspect.signature(get_imr_esigma_waveform).parameters.keys()
 def pycbc_esigma(**params):
     pt = {p: params[p] for p in _params if p in params}
     pt['f_ref'] = None
-    return get_imr_esigma_waveform(pt.pop('mass1'), pt.pop('mass2'),
+
+    gen_wav = get_imr_esigma_waveform
+    if 'skip_merger' in params:
+        gen_wav = get_inspiral_esigma_waveform
+
+    return gen_wav(pt.pop('mass1'), pt.pop('mass2'),
                             pt.pop('f_lower'), pt.pop('delta_t'),
                             **pt, modes_to_use=[(2,2), (2,-2)])
 
